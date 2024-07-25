b2e11eb42c7c592553350567b8a96e922cc53737
Ustinova <rykoca.tanya.96@gmail.com>
2024-07-25 11:01:36 +0300

    style description correction Link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,30 +2,26 @@
 import '../../Pages/HomePage/HomePage.scss'
 
 import shooter from '../../assets/arrow-right.png';
-import { Link } from '@mui/material';
+import Link from '@mui/material/Link';
 import logo from '../../assets/logoImg/Frame 82.png'
 import './Navbar.scss';
 
 
 
-export function Navbar() {
+export const Navbar = () => {
   return (
     <div className='header-container'>
       <img className='logo' src={logo} alt='picture logo' />
       <div className='menu-navigation'>
-        <Link sx={{
-          color:'white',
-        }} href="#" underline="hover">Movie</Link>
-        <Link  sx={{
-          color:'white',
-        }}href="#" underline="hover">Catalog</Link>
-        <Link sx={{
-          color:'white',
-        }} href="#" underline="hover">Sign in <img src={shooter} alt='shooter' /></Link>
+        <Link href="#" className='custom-link' underline="hover">Movie</Link>
+        <Link href="#" className='custom-link' underline="hover">Catalog</Link>
+        <Link href="#" className='custom-link' underline="hover">Sign in <img src={shooter} alt='shooter' /></Link>
+
       </div>
     </div>
 
   )
 }
+export default Navbar
 
 
