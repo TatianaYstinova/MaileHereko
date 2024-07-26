1081785c5ce070eddad060aef277d7867dbdd5d4
Antonina66 <88768298+Antonina66@users.noreply.github.com>
2024-07-26 12:33:11 +0300

    Added a movie card component. Added a common theme for the material-ui

---
+++ src/theme.tsx
@@ -0,0 +1,47 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#7B6EF6",
+    },
+    secondary: {
+      main: "#5A4AF4",
+    },
+    text: {
+      primary: "#EBEEF5",
+    },
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          fontFamily: "Poppins, sans-serif",
+        },
+      },
+    },
+
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "rgba(32, 40, 62, 0.4)",
+          color: "#ffffff",
+        },
+      },
+    },
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "rgba(0, 0, 0, 0.65)",
+          color: "#FFAD49",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
