99b49ec10cc89af90c16312cb407e6d6fb20f2d8
PolinaRudich <rudichrudichrudich@mail.ru>
2024-08-02 00:21:58 +0300

    removed unused vars and files

--- src/Pages/ProfilePage/ProfileClient.tsx
+++
@@ -1,8 +0,0 @@
-import {useEffect,useState } from 'react';
-import { Favourites } from '../../entities/movie/api/types';
-
-function CreateProfileClient (){
-    const [favourites,setFavorites] = useState<Favourites[]>([]);
-
-    
-}
